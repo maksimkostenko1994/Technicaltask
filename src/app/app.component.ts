d2f29b2173c910c895eae1c226a8536936195dac
Kostenko Maksym <maksimmaksim@meta.ua>
2018-07-12 23:08:29 +0300

    Hello user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  templateUrl: './app.greeting.html'
 })
 export class AppComponent {
-  title = 'app';
+  name = '';
 }
